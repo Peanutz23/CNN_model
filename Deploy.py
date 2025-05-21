457e781cac17daef5f3d1c16418ac5b7d7afb4ed
Peanutz23 <130323459+Peanutz23@users.noreply.github.com>
2025-05-21 22:02:12 +0800

    Update Deploy.py

--- Deploy.py
+++ Deploy.py
@@ -12,5 +12,7 @@ if uploaded:
     img = Image.open(uploaded).convert("RGB")
     img_resized = img.resize((32, 32))
     img_array = np.array(img_resized).astype("float32") / 255.0
+    class_names = ['airplane', 'automobile', 'bird', 'cat', 'deer',
+               'dog', 'frog', 'horse', 'ship', 'truck']
     prediction = model.predict(np.expand_dims(img_array, axis=0))
     st.write("Prediction:", np.argmax(prediction))
