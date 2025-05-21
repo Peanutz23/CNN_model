ea538db786836a3533196c81f9c798d176f3c93f
Peanutz23 <130323459+Peanutz23@users.noreply.github.com>
2025-05-21 22:12:13 +0800

    Update Deploy.py

--- Deploy.py
+++ Deploy.py
@@ -10,17 +10,21 @@ model = tf.keras.models.load_model("CNN_model.h5")
 class_names = ['airplane', 'automobile', 'bird', 'cat', 'deer',
                'dog', 'frog', 'horse', 'ship', 'truck']
 
-st.title("Vehicle Classifier")
+st.title("CIFAR Classifier")
 uploaded = st.file_uploader("Upload an image", type=["jpg", "png", "jpeg"])
 
 if uploaded:
-    img = Image.open(uploaded).convert("RGB")
-    img_resized = img.resize((32, 32))
+    # Display the uploaded image
+    image = Image.open(uploaded).convert("RGB")
+    st.image(image, caption="Uploaded Image", use_column_width=True)
+
+    # Preprocess the image
+    img_resized = image.resize((32, 32))
     img_array = np.array(img_resized).astype("float32") / 255.0
     prediction = model.predict(np.expand_dims(img_array, axis=0))
-    
+
     # Get predicted label
     predicted_index = np.argmax(prediction)
     predicted_label = class_names[predicted_index]
-    
-    st.write("Prediction:", predicted_label)
+
+    st.write("### Prediction:", predicted_label)
